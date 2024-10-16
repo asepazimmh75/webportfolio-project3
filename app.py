586caf4cd707d7aa0d7c59461dbf8449906406ea
asepazimmh75 <asepazizmiftahulhuda75@gmail.com>
2024-10-17 01:42:42 +0700

    Website Portfolio Deployment

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+## Setiap URL memiliki nama fungsi yang sama,
+## Alama-alamat pada route('/'), dll. harus tidak boleh sama.
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=10000, debug=True)
